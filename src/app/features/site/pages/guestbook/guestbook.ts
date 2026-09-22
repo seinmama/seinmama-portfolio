4e61578b16c877f1efb9939d8900ef5d5d201cce
Zune Zune <seinmama95@gmail.com>
2026-09-22 23:03:56 +0700

    address box form

--- src/app/features/site/pages/guestbook/guestbook.ts
+++ src/app/features/site/pages/guestbook/guestbook.ts
@@ -1,10 +1,11 @@
 import { Component, inject, signal } from '@angular/core';
+import { DatePipe } from '@angular/common';
 import { FormsModule } from '@angular/forms';
 import { GuestbookService } from '../../../../core/guestbook.service';
 
 @Component({
   selector: 'page-guestbook',
-  imports: [FormsModule],
+  imports: [FormsModule, DatePipe],
   templateUrl: './guestbook.html',
   styleUrl: './guestbook.less',
 })
